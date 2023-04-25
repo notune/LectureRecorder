4f2b84efe282980f4ba08a0dc057ac467c42942d
Noah <noahmuehl@me.com>
2023-04-25 18:51:24 +0200

    (wip) add recording functionality

--- lecture_recorder/lib/main.dart
+++ lecture_recorder/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_sound/flutter_sound.dart';
 import 'package:pdf_render/pdf_render.dart';
 import 'package:pdf_render/pdf_render_widgets.dart';
 import 'package:path_provider/path_provider.dart';
@@ -6,6 +8,7 @@ import 'package:video_player/video_player.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_ffmpeg/flutter_ffmpeg.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:flutter_sound_platform_interface/flutter_sound_recorder_platform_interface.dart';
 import 'dart:io';
 
 void main() {
@@ -34,6 +37,61 @@ class _LectureRecorderState extends State<LectureRecorder> {
   PdfDocumentLoader? _pdfDocumentLoader;
   PdfDocument? _pdfDocument;
   int _currentPageIndex = 0;
+  FlutterSoundRecorder? _audioRecorder;
+  bool _recorderIsInited = false;
+  bool _isRecording = false;
+  String _audioPath =
+      'audio_record.mp3'; // You can choose your preferred audio format
+
+  @override
+  void initState() {
+    super.initState();
+    _initAudioRecorder();
+  }
+
+  @override
+  void dispose() {
+    _audioRecorder?.closeRecorder();
+    super.dispose();
+  }
+
+  Future<void> _initAudioRecorder() async {
+    _audioRecorder = FlutterSoundRecorder();
+
+    // Request microphone permission
+    if (!kIsWeb) {
+      var status = await Permission.microphone.request();
+      if (status != PermissionStatus.granted) {
+        throw RecordingPermissionException('Microphone permission not granted');
+      }
+    }
+
+    // Initialize the recorder
+    await _audioRecorder!.openRecorder();
+    setState(() {
+      _recorderIsInited = true;
+    });
+  }
+
+  void _startRecording() async {
+    if (!_recorderIsInited) return;
+    await _audioRecorder!.startRecorder(
+      toFile: _audioPath,
+      codec: Codec.mp3, // Change this to your preferred codec
+      audioSource: AudioSource.microphone,
+    );
+    setState(() {
+      _isRecording = true;
+    });
+  }
+
+  void _stopRecording() async {
+    if (!_isRecording) return;
+    await _audioRecorder!.stopRecorder();
+    setState(() {
+      _isRecording = false;
+    });
+  }
 
   // Other variables and methods for handling audio, video, and recording will be placed here.
   // ...
@@ -58,6 +116,21 @@ class _LectureRecorderState extends State<LectureRecorder> {
     }
   }
 
+  void _mergeAudioAndVideo(String videoPath, String audioPath) async {
+    final FlutterFFmpeg _flutterFFmpeg = FlutterFFmpeg();
+    String outputPath = 'merged_output.mp4'; // Output file path
+    int returnCode = await _flutterFFmpeg.execute(
+        '-i $videoPath -i $audioPath -c copy -map 0:v:0 -map 1:a:0 $outputPath');
+
+    if (returnCode == 0) {
+      // Success
+      print('Merged video and audio successfully: $outputPath');
+    } else {
+      // Error
+      print('Error merging video and audio: $returnCode');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -68,6 +141,15 @@ class _LectureRecorderState extends State<LectureRecorder> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  onPressed: _isRecording ? _stopRecording : _startRecording,
+                  icon: Icon(_isRecording ? Icons.stop : Icons.mic),
+                ),
+              ],
+            ),
             if (_pdfDocumentLoader != null)
               Expanded(
                 child: Center(

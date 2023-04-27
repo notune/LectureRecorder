662cccc879864496cf8c50078a1dc10bcc6ecbb4
Noah Mühl <noahmuehl@me.com>
2023-04-27 15:53:03 +0200

    store settings in sharedprefs

--- lecture_recorder/lib/settings.dart
+++ lecture_recorder/lib/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Settings extends StatefulWidget {
   const Settings({Key? key}) : super(key: key);
@@ -10,6 +11,26 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   String _videoQuality = 'medium';
   bool _wakelockWhileRecording = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadSettings();
+  }
+
+  Future<void> _saveSettings() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString('videoQuality', _videoQuality);
+    await prefs.setBool('wakelockWhileRecording', _wakelockWhileRecording);
+  }
+
+  Future<void> _loadSettings() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _videoQuality = prefs.getString('videoQuality') ?? 'medium';
+      _wakelockWhileRecording = prefs.getBool('wakelockWhileRecording') ?? true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +53,7 @@ class _SettingsState extends State<Settings> {
                 setState(() {
                   _videoQuality = newValue!;
                 });
+                _saveSettings();
               },
             ),
           ),
@@ -43,6 +65,7 @@ class _SettingsState extends State<Settings> {
                 setState(() {
                   _wakelockWhileRecording = newValue;
                 });
+                _saveSettings();
               },
             ),
           ),
--- lecture_recorder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ lecture_recorder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import ffmpeg_kit_flutter
 import path_provider_foundation
 import pdf_render
 import share_plus
+import shared_preferences_foundation
 import wakelock_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -16,5 +17,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SwiftPdfRenderPlugin.register(with: registry.registrar(forPlugin: "SwiftPdfRenderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   WakelockMacosPlugin.register(with: registry.registrar(forPlugin: "WakelockMacosPlugin"))
 }

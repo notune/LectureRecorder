411b02f334eed117e0afd4c7c85756e95d153536
Noah Mühl <noahmuehl@me.com>
2023-05-18 01:27:21 +0200

    change to GPL license

--- lecture_recorder/lib/settings.dart
+++ lecture_recorder/lib/settings.dart
@@ -1,18 +1,19 @@
-/*
- * Copyright 2023 Noah Mühl
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/* 
+This file is part of Lecture Recorder.
+
+Lecture Recorder is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Lecture Recorder is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Lecture Recorder. If not, see <https://www.gnu.org/licenses/>. 
+*/
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
